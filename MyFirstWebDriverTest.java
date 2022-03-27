e2bfb78ed6476c2dad5fd19bd214484307c1252b
Kavita Mehta <kavi.arrora@gmail.com>
2022-03-27 13:20:52 +0530

    Theses are the changes done in MyFirstWebDriver file

---
+++ MyFirstWebDriverTest.java
@@ -0,0 +1,23 @@
+package scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstWebDriverTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver","test\\resources\\chromedriver.exe");
+		
+			
+		WebDriver driver=new ChromeDriver();
+		
+		
+		driver.get("www.google.com");
+
+	}
+
+}
+
