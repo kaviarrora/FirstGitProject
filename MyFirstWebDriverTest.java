ed7522121257ff01260fe53bb56bc23084cee5f0
Kavita Mehta <kavi.arrora@gmail.com>
2022-04-02 16:33:41 +0530

    track comments

--- MyFirstWebDriverTest.java
+++ MyFirstWebDriverTest.java
@@ -3,6 +3,7 @@ package scripts;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//mark these
 public class MyFirstWebDriverTest {
 
 	public static void main(String[] args) {
